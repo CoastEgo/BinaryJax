22f9147a118805db2f346bd3ce196cd63a16ea02
CoastEgo <Coas_ego@outlook.com>
2023-03-26 19:56:40 +0800

    修改gitignore

--- numpy_version/example.py
+++ numpy_version/example.py
@@ -8,26 +8,32 @@ import sys
 import time
 sys.setrecursionlimit(10000)
 np.seterr(divide='ignore', invalid='ignore')
-start=time.perf_counter()
 if __name__=="__main__":
-    b=0.5789473684210527
+    b_map=np.linspace(-0.08,0.04,120)
+    b=b_map[79]
     t_0=2452848.06;t_E=61.5
-    q=0.1;alphadeg=90;s=1.5;rho=0.5
-    trajectory_n=30
-    alpha=alphadeg*2*np.pi/360;delta_x=s/(1+q);m1=1/(1+q);m2=q/(1+q);posi=np.array([0,0])##m1较大 m2较小
+    q=1e-4;alphadeg=90;s=1.0;rho=1e-3
+    trajectory_n=300
+    alpha=alphadeg*2*np.pi/360
+    start=time.perf_counter()
     times=np.linspace(t_0-1.5*t_E,t_0+1.5*t_E,trajectory_n)
     model_uniform=model({'t_0': t_0, 'u_0': b, 't_E': t_E,
                         'rho': rho, 'q': q, 's': s, 'alpha_deg': alphadeg,'times':times})
-    model_1S2L = Model({'t_0': t_0, 'u_0': b, 't_E': t_E,
-                        'rho': rho, 'q': q, 's': s, 'alpha': alphadeg})
-    model_1S2L.set_magnification_methods([t_0-1.5*t_E, 'VBBL',t_0+1.5*t_E])
-    Mulens_mag=model_1S2L.get_magnification(time=times)
     uniform_mag=model_uniform.get_magnifaction()
     end=time.perf_counter()
-    print(end-start)
+    print('uniform=',end-start)
+    start=time.perf_counter()
+    model_1S2L = Model({'t_0': t_0, 'u_0': b, 't_E': t_E,
+                        'rho': rho, 'q': q, 's': s, 'alpha': alphadeg})
+    model_1S2L.set_magnification_methods([t_0-1.6*t_E, 'VBBL',t_0+1.5*t_E])
+    model_1S2L.set_magnification_methods_parameters({'VBBL': {'accuracy': 1e-3}})
+    Mulens_mag=model_1S2L.get_magnification(time=times)
+    end=time.perf_counter()
+    print('mulensmodel=',end-start)
     bool2=1
-    if bool2:
+    if 0:
         fig=plt.figure(figsize=(6,6))
+        #axis=plt.axes()
         axis=plt.axes(xlim=(-2,2),ylim=(-2,2))
         model_1S2L.plot_trajectory()
         caustic_1=caustics.Caustics(q,s)
@@ -42,11 +48,10 @@ if __name__=="__main__":
         plt.plot(times,Mulens_mag,label='MulensModel')
         plt.plot(times,uniform_mag,label='uniform')
         plt.legend()
-        plt.savefig('picture/magnification.png')
-    if bool2:
+        plt.savefig('picture/magnification_optimal.png')
+    if 1:
         plt.figure('de-mag')
-        plt.plot(times,np.abs(Mulens_mag-uniform_mag)/Mulens_mag,label='$\Delta$')
-        delta=np.abs(Mulens_mag-uniform_mag)/Mulens_mag
-        print(np.argmax(delta))
+        plt.plot(times,np.abs((Mulens_mag-uniform_mag)),label='$\Delta$_optimal')
+        plt.yscale('log')
         plt.legend()
-        plt.savefig('picture/delta_magnification.png')
+        plt.savefig('picture/delta_mag_optimal.png')
