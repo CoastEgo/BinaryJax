590eb21ce9f7533984f4d1a3c50513f71d4f972d
CoastEgo <Coas_ego@outlook.com>
2023-04-21 15:21:53 +0800

    jac计算

--- numpy_version/jacobian_calculate.py
+++ numpy_version/jacobian_calculate.py
@@ -1,15 +1,15 @@
 import numpy as np
 from uniform_model_numpy import model
 import sys
-import time
+from jax import jacfwd
 sys.setrecursionlimit(10000)
 #np.seterr(divide='ignore', invalid='ignore')
 def uniform(input,times):
     t_0,t_E,q,s,alphadeg,b,rho=input
-    sample_n=50
+    tol=1e-2
     model_uniform=model({'t_0': t_0, 'u_0': b, 't_E': t_E,
-                        'rho': rho, 'q': q, 's': s, 'alpha_deg': alphadeg,'times':times,'sample_n':sample_n})
-    uniform_mag=model_uniform.mag
+                        'rho': rho, 'q': q, 's': s, 'alpha_deg': alphadeg,'times':times})
+    uniform_mag=model_uniform.get_magnifaction(tol)
     return uniform_mag
 def partial(input,times,delta=0.00001):
     jacobian=np.zeros((len(times),(len(input))))
@@ -25,4 +25,7 @@ t_E=61.5
 trajectory_n=10
 times=np.linspace(t_0-1.5*t_E,t_0+1.5*t_E,trajectory_n)
 jac=partial(input,times)
-np.savetxt('jacobian.txt',jac,delimiter=',',fmt='%1.4f')
+np.savetxt('result/jacobian.txt',jac,delimiter=',',fmt='%1.4f')
+jacobian_fn = jacfwd(uniform, argnums=0)  # it returns the function in charge of computing jacobian
+jacobian = jacobian_fn(input,times)
+np.savetxt('result/jacobian_auto.txt',jacobian,delimiter=',',fmt='%1.4f')
