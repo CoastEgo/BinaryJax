9a3db5f1c474cd113d5a45dc4c207f6d262dbf35
CoastEgo <Coas_ego@outlook.com>
2023-06-16 00:52:21 +0800

    jit 初始化函数

--- jax_version/uniform_model_jax.py
+++ jax_version/uniform_model_jax.py
@@ -7,62 +7,72 @@ from solution import *
 from basic_function_jax import Quadrupole_test
 jax.config.update("jax_enable_x64", True)
 jax.config.update("jax_platform_name", "cpu")
-class model():#initialize parameter
-    def __init__(self,par):
-        self.t_0=par['t_0']
-        self.u_0=par['u_0']
-        self.t_E=par['t_E']
-        self.rho=par['rho']
-        self.q=par['q']
-        self.s=par['s']
-        self.alpha_rad=par['alpha_deg']*2*jnp.pi/360
-        self.times=(par['times']-self.t_0)/self.t_E
-        self.trajectory_n=len(self.times)
-        self.m1=1/(1+self.q)
-        self.m2=self.q/(1+self.q)
-        self.trajectory_l=self.get_trajectory_l()
-    def to_centroid(self,x):#change coordinate system to cetorid
-        delta_x=self.s/(1+self.q)
-        return -(jnp.conj(x)-delta_x)
-    def to_lowmass(self,x):#change coordinaate system to lowmass
-        delta_x=self.s/(1+self.q)
-        return -jnp.conj(x)+delta_x
-    def get_trajectory_l(self):
-        alpha=self.alpha_rad
-        b=self.u_0
-        trajectory_l=self.to_lowmass(self.times*jnp.cos(alpha)-b*jnp.sin(alpha)+1j*(b*jnp.cos(alpha)+self.times*jnp.sin(alpha)))
-        return trajectory_l
-    def get_magnifaction2(self,tol,retol=0):
-        trajectory_l=self.trajectory_l
-        trajectory_n=self.trajectory_n
-        zeta_l=trajectory_l[:,None]
-        coff=get_poly_coff(zeta_l,self.s,self.m2)
-        z_l=get_roots(trajectory_n,coff)
-        error=verify(zeta_l,z_l,self.s,self.m1,self.m2)
-        cond=error<1e-6
-        index=jnp.where((cond.sum(axis=1)!=3) & (cond.sum(axis=1)!=5))[0]
-        if index.size!=0:
-            sortidx=jnp.argsort(error[index],axis=1)
-            cond.at[index].set(False)
-            cond.at[index,sortidx[0:3]].set(True)
-        z=jnp.where(cond,z_l,jnp.nan)
-        zG=jnp.where(cond,jnp.nan,z_l)
-        cond,mag=Quadrupole_test(self.rho,self.s,self.q,zeta_l,z,zG,tol)
-        idx=jnp.where(~cond)[0]
-        carry,_=lax.scan(contour_scan,(mag,trajectory_l,tol,retol,self.rho,self.s,self.q,self.m1,self.m2,
-                                       jnp.array([0]),jnp.zeros((200,1)),False),idx)
-        mag,trajectory_l,tol,retol,rho,s,q,m1,m2,sample_n,error_hist,outloop=carry
-        #print(jnp.nansum(error_hist))
-        return mag
-def contour_scan(carry,i):
-    mag_all,trajectory_l,tol,retol,rho,s,q,m1,m2,sample_n,error_hist,outloop=carry
-    result=contour_integrate(rho,s,q,m1,m2,trajectory_l[i],tol,epsilon_rel=retol)
-    (sample_n,theta,error_hist,roots,parity,ghost_roots_dis,buried_error,sort_flag,
-        Is_create,_,rho,s,q,m1,m2,epsilon,epsilon_rel,mag,maglast,outloop)=result
-    mag_all=mag_all.at[i].set(mag[0])
-    return (mag_all,trajectory_l,tol,retol,rho,s,q,m1,m2,sample_n,error_hist,outloop),i
 @jax.jit
-def contour_integrate(rho,s,q,m1,m2,trajectory_l,epsilon,epsilon_rel=0,inite=40,n_ite=200):
+def model(par):
+    t_0=par['t_0']; u_0=par['u_0']; t_E=par['t_E']
+    rho=par['rho']
+    q=par['q']
+    s=par['s']
+    alpha_rad=par['alpha_deg']*2*jnp.pi/360
+    times=(par['times']-t_0)/t_E
+    retol=par['retol']
+    trajectory_n=len(times)
+    m1=1/(1+q)
+    m2=q/(1+q)
+    trajectory_l=get_trajectory_l(s,q,alpha_rad,u_0,times)
+    ###四极测试
+    zeta_l=trajectory_l[:,None]
+    coff=get_poly_coff(zeta_l,s,m2)
+    z_l=get_roots(trajectory_n,coff)
+    error=verify(zeta_l,z_l,s,m1,m2)
+    cond=error<1e-6
+    ambigious_cond=~((cond.sum(axis=1)!=3) & (cond.sum(axis=1)!=5))
+    '''index=jnp.where((cond.sum(axis=1)!=3) & (cond.sum(axis=1)!=5))[0]
+    if index.size!=0:
+        sortidx=jnp.argsort(error[index],axis=1)
+        cond=cond.at[index].set(False)
+        cond=cond.at[index,sortidx[0:3]].set(True)'''
+    index=jnp.where((cond.sum(axis=1)!=3) & (cond.sum(axis=1)!=5),size=int(trajectory_n/2)+1,fill_value=-1)[0]
+    def ambigious_deal(carry):
+        error,index,cond=carry
+        sortidx=jnp.argsort(error[index],axis=1)
+        cond=cond.at[index].set(False)
+        cond=cond.at[index,sortidx[:,0:3]].set(True)
+        return cond
+    cond=lax.cond((index!=-1).any(),ambigious_deal,lambda x : x[-1],(error,index,cond))
+    z=jnp.where(cond,z_l,jnp.nan)
+    zG=jnp.where(cond,jnp.nan,z_l)
+    cond,mag=Quadrupole_test(rho,s,q,zeta_l,z,zG,retol)
+    cond=cond&(ambigious_cond)
+    ###
+    carry,_=lax.scan(contour_scan,(mag,trajectory_l,retol,retol,rho,s,q,m1,m2,
+                                    jnp.array([0]),cond,jnp.zeros((400,1)),False),jnp.arange(trajectory_n))
+    mag,trajectory_l,tol,retol,rho,s,q,m1,m2,sample_n,cond,error_hist,outlop=carry
+    return mag
+def to_centroid(s,q,x):#change coordinate system to cetorid
+    delta_x=s/(1+q)
+    return -(jnp.conj(x)-delta_x)
+def to_lowmass(s,q,x):#change coordinaate system to lowmass
+    delta_x=s/(1+q)
+    return -jnp.conj(x)+delta_x
+def get_trajectory_l(s,q,alpha_rad,u_0,times):
+    alpha=alpha_rad
+    b=u_0
+    trajectory_l=to_lowmass(s,q,times*jnp.cos(alpha)-b*jnp.sin(alpha)+1j*(b*jnp.cos(alpha)+times*jnp.sin(alpha)))
+    return trajectory_l
+def contour_scan(carry,i):
+    mag_all,trajectory_l,retol,retol,rho,s,q,m1,m2,sample_n,cond,error_hist,outlop=carry
+    def false_fun(carry):
+        mag_all,trajectory_l,retol,retol,rho,s,q,m1,m2,sample_n,cond,error_hist,outlop=carry
+        result=contour_integrate(rho,s,q,m1,m2,trajectory_l[i],retol,epsilon_rel=retol)
+        (sample_n,theta,error_hist,roots,parity,ghost_roots_dis,buried_error,sort_flag,
+            Is_create,_,rho,s,q,m1,m2,epsilon,epsilon_rel,mag,maglast,outloop)=result
+        mag_all=mag_all.at[i].set(mag[0])
+        return mag_all,sample_n,error_hist,outlop
+    mag_all,sample_n,error_hist,outlop=lax.cond(cond[i].all(),lambda x:(x[0],x[-4],x[-2],x[-1]),false_fun,carry)
+    return (mag_all,trajectory_l,retol,retol,rho,s,q,m1,m2,sample_n,cond,error_hist,outlop),i
+@jax.jit
+def contour_integrate(rho,s,q,m1,m2,trajectory_l,epsilon,epsilon_rel=0,inite=30,n_ite=400):
     ###初始化
     sample_n=jnp.array([inite])
     theta=jnp.where(jnp.arange(n_ite)<inite,jnp.resize(jnp.linspace(0,2*jnp.pi,inite),n_ite),jnp.nan)[:,None]#shape(500,1)
