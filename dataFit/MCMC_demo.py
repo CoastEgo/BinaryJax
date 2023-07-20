a386c5154dbe1c91c81499592646d6aea55cd4b6
CoastEgo <Coas_ego@outlook.com>
2023-07-20 20:33:45 +0800

    mcmc demo for grid search

--- dataFit/MCMC_demo.py
+++ dataFit/MCMC_demo.py
@@ -7,13 +7,12 @@ import jax
 import jax.numpy as jnp
 from ..binaryJax import model
 import VBBinaryLensing
+numpyro.enable_x64()
 # 生成数据
 np.random.seed(0)
-b=0.2
-t_0=2452848.06;t_E=61.5;alphadeg=90
-q=0.04;s=1.3;rho=0.009
+b=0.2;t_0=2452848.06;t_E=61.5;alphadeg=90;q=0.04;s=1.3;rho=0.009
 tol=1e-2
-trajectory_n=50
+trajectory_n=500
 VBBL = VBBinaryLensing.VBBinaryLensing()
 alpha_VBBL=np.pi+alphadeg/180*np.pi
 VBBL.RelTol=tol
@@ -29,30 +28,34 @@ VBBL_mag*=(1+ np.random.normal(0, 0.001, size=VBBL_mag.shape))
 ##
 alpha=alphadeg*2*jnp.pi/360
 times=jnp.linspace(t_0-2.*t_E,t_0+2.*t_E,trajectory_n)
-mean=model({'t_0': t_0, 'u_0': b, 't_E': t_E,
-                    'rho': rho, 'q': q, 's': s, 'alpha_deg': alphadeg,'times':times,'retol':tol})
-print('jit complete')
 def model_MCMC(times, mag):
-    t_0=2452848.06;t_E=61.5;b=0.2
-    alphadeg=90;tol=1e-2
-    #q=0.04;s=1.3
-    q_range = numpyro.sample('q_range', dist.Uniform(-3, 0.))
+    t_0=2452848.06
+    tol=0.01
+    ###参数采样
+    alphadeg_range=numpyro.sample('alphadeg_range', dist.Uniform(0., 1))
+    t_E_range=numpyro.sample('t_E_range', dist.Uniform(0., 1))
+    q_range = numpyro.sample('q_range', dist.Uniform(-1.4, -1.25))
+    rho_range= numpyro.sample('rho_range',dist.Uniform(-2.3, -2.0))
+    #参数计算
+    alphadeg=numpyro.deterministic('alphadeg',alphadeg_range*360)
+    b=numpyro.sample('b', dist.Uniform(0,2))
     q= numpyro.deterministic('q',10**q_range)
-    s = numpyro.sample('s', dist.Uniform(0.8, 1.3))
-    rho_range= numpyro.sample('rho_range',dist.Uniform(-3, 0.))
+    s = numpyro.sample('s', dist.Uniform(1.28,1.31))
+    t_E=numpyro.deterministic('t_E',100*t_E_range)
     rho= numpyro.deterministic('rho',10**rho_range)
     mean=model({'t_0': t_0, 'u_0': b, 't_E': t_E,
                         'rho': rho, 'q': q, 's': s, 'alpha_deg': alphadeg,'times':times,'retol':tol})
-    sigma = numpyro.sample('sigma', dist.Exponential(1))
-    numpyro.sample('obs', dist.Normal(mean, sigma), obs=mag)
-init_strategy=numpyro.infer.init_to_value(values={'q_range':-1.,'s':1.0,'rho_range':-1.})#'q_range':0.,'s':1.0,
+    numpyro.sample('obs', dist.Normal(mean, tol*mean), obs=mag)
+init_strategy=numpyro.infer.init_to_value(values={'alphadeg_range':0.5,'t_E_range':0.5,'b':1.,'q_range':-1.26,'s':1.281,'rho_range':-2.2})#'q_range':0.,'s':1.0,
 # 运行 MCMC 推断
-nuts_kernel = NUTS(model_MCMC,init_strategy=init_strategy,forward_mode_differentiation=True)
+nuts_kernel = NUTS(model_MCMC,step_size=0.001,adapt_step_size=False,init_strategy=init_strategy,forward_mode_differentiation=True)
+#,target_accept_prob=0.9
 mcmc = MCMC(nuts_kernel, num_samples=1000, num_warmup=500,jit_model_args=True)
 mcmc.run(jax.random.PRNGKey(0), times=times, mag=VBBL_mag)
 posterior_samples = mcmc.get_samples()
 
 # 获取后验分布的均值和标准差
+mcmc.print_summary(exclude_deterministic=False)
 q_mean = np.mean(posterior_samples['q'])
 q_std = np.std(posterior_samples['q'])
 s_mean = np.mean(posterior_samples['s'])
