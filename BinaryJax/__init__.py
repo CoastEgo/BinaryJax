abc43d361103e9ffc5b6d8815a3016aa9747147b
CoastEgo <Coast_ego@outlook.com>
2024-07-16 15:13:49 +0800

    support point_light_curve import

--- BinaryJax/__init__.py
+++ BinaryJax/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-all=['model','model_noimage','model_numpy','contour_integrate','to_lowmass','to_centroid']
+all=['model','point_light_curve','model_noimage','model_numpy','contour_integrate','to_lowmass','to_centroid']
 
-from .binaryJax.model_jax import model
+from .binaryJax.model_jax import model,point_light_curve
 from .binaryJax.model_jax import contour_integrate,to_lowmass,to_centroid
 from .binaryNumpy.model_noimage import model as model_noimage
 from .binaryNumpy.model_numpy import model as model_numpy
